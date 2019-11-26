93146b0fcfe6646bf49559b8718179c3dea1a800
Igor Rozum <irozum@greengeeks.com>
2019-11-26 18:08:38 -0500

    import line removed from app.js

--- routes/User.js
+++ routes/User.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const User = require('../models/User')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs')
-const session = require('express-session')
 const path = require('path')
 
 router.use(bodyParser.json());
